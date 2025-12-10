bcc33aa2bbd74898192fd88baaabb38c1f175ce1
AriRoss26 <230979306+AriRoss26@users.noreply.github.com>
2025-12-10 18:37:34 +0700

    добавление графики

--- semestr/src/main/java/com/semestr/Board.java
+++ semestr/src/main/java/com/semestr/Board.java
@@ -1,5 +1,39 @@
 package com.semestr;
 
-public class Board {
-    
+import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+//наследование от Parent, чтобы этот объект можно было добавить в окно
+public class Board extends Parent {
+    //VBox будет хранить 10 горизонтальных строк
+    private VBox rows = new VBox();
+    public Board() {
+        //цикл по строкам у
+        for (int y = 0; y < 10; y++) {
+            HBox row = new HBox(); //создание одной строки
+            //цикл по столбцам х
+            for (int x = 0; x < 10; x++) {
+                Cell c = new Cell(x, y);
+                row.getChildren().add(c); //добавление клетки в строку
+            }
+            rows.getChildren().add(row); //добавление готовой строки в столбец
+        }
+        //добавление всей конструкции в сам объект Board
+        getChildren().add(rows);
+    }
+    //внутренний класс Клетка
+    public class Cell extends Rectangle {
+        public int x, y;
+        public Cell(int x, int y) {
+            super(30, 30); //размер клетки 30x30 пикселей
+            this.x = x;
+            this.y = y;
+            //настройка внешнего вида
+            setFill(Color.LIGHTBLUE); //цвет воды
+            setStroke(Color.BLACK);   //чёрная рамка
+        }
+    }
 }
--- semestr/src/main/java/com/semestr/Test1.java
+++ semestr/src/main/java/com/semestr/Test1.java
@@ -1,5 +1,25 @@
 package com.semestr;
 
-public class Test1 {
-    
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Test1 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        //создание доски
+        Board testBoard = new Board();
+        //кладем доску в контейнер
+        StackPane root = new StackPane(testBoard);
+        //cоздание сцены
+        Scene scene = new Scene(root, 400, 400);
+        //настраиваем и показываем окно
+        primaryStage.setTitle("Шаг 1: Тест Доски");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
